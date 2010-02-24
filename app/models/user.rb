93af5594e9d11fed89aca6e89c80fb4af9ca482d
Mathias <mathiasmex@gmail.com>
2010-02-24 09:13:31 +0100

    User model with passwords, and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 # == Schema Information
+# Schema version: 20100224052042
+#
+# Table name: users
+#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#
+
+# == Schema Information
 # Schema version: 20100113164536
 #
 # Table name: users
@@ -9,9 +23,10 @@
 #  created_at :datetime
 #  updated_at :datetime
 #
+require 'digest'
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessible :name, :email, :password, :password_confirmation
 
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -19,4 +34,40 @@ class User < ActiveRecord::Base
   validates_length_of     :name, :maximum => 50
   validates_format_of     :email, :with => EmailRegex
   validates_uniqueness_of :email, :case_sensitive => false
+
+  validates_confirmation_of :password
+
+  validates_presence_of :password
+  validates_length_of   :password, :within => 6..40
+
+  before_save :encrypt_password
+
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
+  private
+
+    def encrypt_password
+      self.salt = make_salt
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+# By using the symbol ':user', we get Factory Girl to simulate the User model.
+Factory.define :user do |user|
+  user.name                  "Mathias Sasse"
+  user.email                 "mathiasmex@sampleapp.com"
+  user.password              "yelapamex"
+  user.password_confirmation "yelapamex"
+end
